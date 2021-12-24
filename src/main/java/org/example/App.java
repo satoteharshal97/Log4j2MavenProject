f172002670ed08af9cb5b7f7d6ac1ea365eeb187
satoteharshal97 <37376689+satoteharshal97@users.noreply.github.com>
2021-12-24 17:59:56 +0530

    Project Completed

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,24 @@
+package org.example;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    private static final Logger logger = LogManager.getLogger(App.class);
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        logger.trace("We've just greeted the user!");
+        logger.debug("We've just greeted the user!");
+        logger.info("We've just greeted the user!");
+        logger.warn("We've just greeted the user!");
+        logger.error("We've just greeted the user!");
+        logger.fatal("We've just greeted the user!");
+    }
+
+
+}
